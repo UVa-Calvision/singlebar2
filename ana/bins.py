7d799e7da7a92d7d3f3b7e99c51fb5682dad3459
Hayden Hollenbeck <kcr8wx@virginia.edu>
2023-03-20 14:50:47 -0400

    Scintillation Photon Hit Collection

--- ana/bins.py
+++ ana/bins.py
@@ -2,18 +2,69 @@
 
 from __future__ import print_function
 import ROOT as r
+import numpy as np
 import os, sys
+
+def makeGraph(ys, offset, length):
+  xs = np.arange(offset, offset + length, length / float(ys.shape[0]))
+  return r.TGraph(len(ys), xs, ys)
 
 f = r.TFile("test.root")
 tree = f.tree
+
+f_frontOffset = 0.
+f_crystalLength = 0.
+f_energy = np.array([])
+
+r_energy = np.array([])
+r_frontOffset = 0.
+r_crystalLength = 0.
 
 target_index = 3
 i = 0
 for event in tree:
   if i == target_index:
-    total_energy = sum(event.ECAL_f_scin_bin_energy)
-    print("Total energy: %f" % total_energy)
-    print("Deposited energy: %f" % event.ECAL_f_depositedEnergy)
-    print("Deposited ionizing energy: %f" % event.ECAL_f_depositedIonEnergy)
+    print("Found bin {}".format(i))
+    print("Front:")
+    f_energy = np.array(event.ECAL_f_scin_bin_energy).flatten('C').astype('float')
+    print(f_energy.shape[0])
+    f_frontOffset = event.ECAL_f_frontOffset
+    print(f_frontOffset)
+    f_crystalLength = event.ECAL_f_crystalLength
+    print(f_crystalLength)
 
+    print("Rear:")
+    r_energy = np.array(event.ECAL_r_scin_bin_energy).flatten('C').astype('float')
+    print(r_energy.shape[0])
+    r_frontOffset = event.ECAL_r_frontOffset
+    print(r_frontOffset)
+    r_crystalLength = event.ECAL_r_crystalLength
+    print(r_crystalLength)
+
+    # total_energy = sum(event.ECAL_f_scin_bin_energy)
+    # print("Total energy: %f" % total_energy)
+    # print("Deposited energy: %f" % event.ECAL_f_depositedEnergy)
+    # print("Deposited ionizing energy: %f" % event.ECAL_f_depositedIonEnergy)
   i += 1
+
+r_frontOffset -= f_frontOffset
+f_frontOffset -= f_frontOffset
+
+tc = r.TCanvas()
+mg = r.TMultiGraph("mg", "Total Scintillation Energy Per Bin;z [mm];E [GeV]")
+
+g_f = makeGraph(f_energy, f_frontOffset, f_crystalLength)
+g_r = makeGraph(r_energy, r_frontOffset, r_crystalLength)
+
+mg.Add(g_f)
+mg.Add(g_r)
+mg.Draw("AL")
+
+tc.Update()
+
+print('Hit return to exit')
+sys.stdout.flush() 
+if sys.version_info.major==2:
+    raw_input('')
+else:
+    input()
--- include/SD_CrystalBase.hh
+++ include/SD_CrystalBase.hh
@@ -15,7 +15,7 @@ class SD_CrystalBase : public SD_Base<SD_CrystalBase<Impl>> {
 
 public:
   SD_CrystalBase(const G4String& name, G4double length, G4double z_offset)
-    : BaseType(name), SPCollection(100, length, z_offset)
+    : BaseType(name), crystalLength(length), frontOffset(z_offset)
   {
     // xtal branches and histograms
     b_depositedEnergy = CreateTree::Instance()->createBranch<float>(Impl::ID + "_depositedEnergy");
@@ -25,20 +25,35 @@ public:
     h_phot_produce_lambda = BaseType::createHistogram("phot_produce_lambda", "Photon lambda production;[nm]", 1250, 0., 1250.);
     h_phot_produce_time = BaseType::createHistogram("phot_produce_time", "Photon time production;[ns]", 500, 0., 50.);
     h_phot_z_pos = BaseType::createHistogram("phot_z_pos", "Photon z production;[nm]", 500, 50., 300.);
-    // h_phot_angle_azimuthal = BaseType::createHistogram("phot_angle_azimuthal", "Photon Azimuthal Angle;[rad]", 100, -M_PI, M_PI);
-    // h_phot_angle_polar = BaseType::createHistogram("phot_angle_polar", "Photon Polar Angle;[rad]", 100, 0., M_PI);
-    // h_phot_init_x = BaseType::createHistogram("phot_init_x", "Photon Init px", 100, -1.0, 1.0);
-    // h_phot_init_y = BaseType::createHistogram("phot_init_y", "Photon Init py", 100, -1.0, 1.0);
+    h_phot_angle_azimuthal = BaseType::createHistogram("phot_angle_azimuthal", "Photon Azimuthal Angle;[deg]", 100, -180., 180.);
+    h_phot_angle_polar = BaseType::createHistogram("phot_angle_polar", "Photon Polar Angle;[deg]", 100, 0., 180.);
+    h_phot_init_x = BaseType::createHistogram("phot_init_x", "Photon Init px", 100, -1.0, 1.0);
+    h_phot_init_y = BaseType::createHistogram("phot_init_y", "Photon Init py", 100, -1.0, 1.0);
     b_scin_bin_total = CreateTree::Instance()->createBranch<std::vector<int>>(Impl::ID + "_scin_bin_total");
     b_scin_bin_energy = CreateTree::Instance()->createBranch<std::vector<float>>(Impl::ID + "_scin_bin_energy");
+    b_crystalLength = CreateTree::Instance()->createBranch<float>(Impl::ID + "_crystalLength");
+    b_frontOffset = CreateTree::Instance()->createBranch<float>(Impl::ID + "_frontOffset");
+
+    // Register hit collection ID
+    auto sd_manager = G4SDManager::GetSDMpointer();
+    sd_manager->AddNewCollection(name, Impl::ID + "_phot_creation");
+    BSPCollectionID = sd_manager->GetCollectionID(Impl::ID + "_phot_creation");
+    BSPCollection = nullptr;
   }
 
-  void Initialize(G4HCofThisEvent*) override {
-    *b_scin_bin_total = std::vector<int>(SPCollection.nBins(), 0);
-    *b_scin_bin_energy = std::vector<float>(SPCollection.nBins(), 0.);
+  void Initialize(G4HCofThisEvent* hc) override {
+    // Can probably not use hit collections and reduce this ID lookup stuff
+    auto sd_manager = G4SDManager::GetSDMpointer();
+    hc->AddHitsCollection(sd_manager->GetCollectionID(Impl::ID + "_phot_creation"), new BinnedScinPhotonCollection(100, crystalLength, frontOffset));
+    BSPCollection = dynamic_cast<BinnedScinPhotonCollection*>(hc->GetHC(BSPCollectionID));
+
+    *b_scin_bin_total = std::vector<int>(BSPCollection->nBins(), 0);
+    *b_scin_bin_energy = std::vector<float>(BSPCollection->nBins(), 0.);
   }
 
   G4bool ProcessHits(G4Step* theStep, G4TouchableHistory* ) override {
+    *b_crystalLength = crystalLength;
+    *b_frontOffset = frontOffset;
 
     // Record energy deposition in the xtal
     const G4double energy = theStep->GetTotalEnergyDeposit();
@@ -61,15 +76,15 @@ public:
           h_phot_produce_lambda[process].Fill(photWL);
           h_phot_produce_time[process].Fill(time);
           h_phot_z_pos[process].Fill(zPos);
-          // h_phot_angle_azimuthal[process].Fill(track->GetMomentum().phi());
-          // h_phot_angle_polar[process].Fill(track->GetMomentum().theta());
+          h_phot_angle_azimuthal[process].Fill(track->GetMomentum().phi() * 180. / M_PI);
+          h_phot_angle_polar[process].Fill(track->GetMomentum().theta() * 180. / M_PI);
 
-          // const auto p = track->GetMomentum();
-          // h_phot_init_x[process].Fill(p.x() / p.mag());
-          // h_phot_init_y[process].Fill(p.y() / p.mag());
+          const auto p = track->GetMomentum();
+          h_phot_init_x[process].Fill(p.x() / p.mag());
+          h_phot_init_y[process].Fill(p.y() / p.mag());
 
           if (process == ProcessType::Scin) {
-            SPCollection.recordCreation(track->GetTotalEnergy(), time, zPos);
+            BSPCollection->recordCreation(track->GetTotalEnergy(), time, zPos);
           }
         }
 
@@ -94,11 +109,11 @@ public:
   // }
 
   void EndOfEvent(G4HCofThisEvent*) override {
-    const auto& bins = SPCollection.GetBins();
+    const auto& bins = BSPCollection->GetBins();
     for (unsigned int i = 0; i < bins.size(); i++) {
-      (*b_scin_bin_total)[i] = bins[i].size();
-      for (const auto& hit : bins[i]) {
-        (*b_scin_bin_energy)[i] += hit.energy / GeV;
+      (*b_scin_bin_total)[i] = bins[i]->GetSize();
+      for (const auto& hit : *(bins[i]->GetVector())) {
+        (*b_scin_bin_energy)[i] += hit->GetEnergy() / GeV;
       }
     }
   }
@@ -130,9 +145,16 @@ private:
   PerProcess<TH1F> h_phot_init_x;
   PerProcess<TH1F> h_phot_init_y;
 
-  ScinPhotonCollection SPCollection;
+  G4int BSPCollectionID;
+  BinnedScinPhotonCollection* BSPCollection;
   std::vector<int>* b_scin_bin_total;
   std::vector<float>* b_scin_bin_energy;
+
+  const G4double crystalLength;
+  const G4double frontOffset;
+
+  float* b_crystalLength;
+  float* b_frontOffset;
 };
 
 #endif
